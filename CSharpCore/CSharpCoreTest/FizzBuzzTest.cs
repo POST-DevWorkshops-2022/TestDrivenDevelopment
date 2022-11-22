f0eb2329293f94f54e0f09e20da412c7db0105ce
Katrin Heiderer <katrin.heiderer@nagarro.com>
2022-11-22 15:47:43 +0100

    add empty FizzBuzz Test

---
+++ CSharpCore/CSharpCoreTest/FizzBuzzTest.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace CSharpCore.Test
+{
+
+    public class FizzBuzzTest
+    {
+        //Write a program that returns the input numbers  
+        //For multiples of three print "Fizz" instead of the number
+        //and for multiples of five print "Buzz" 
+        //For numbers which are multiples of 3 and 5 print "FizzBuzz
+
+        //1 == 1
+        //2 == 2
+        //3 == fizz
+        //4 == 4
+        //5 == buzz
+        //6 == fizz
+        //7 == 7 
+        //8 == 8
+        //9 == fizz
+        //10 == buzz
+        //15 == fizzbuzz
+        //100 == buzz
+
+
+    }
+}
